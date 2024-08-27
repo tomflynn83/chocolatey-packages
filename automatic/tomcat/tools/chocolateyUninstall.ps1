076215345c6f05244b02c61e48c4cda99fe08855
nicoske <nicoske@gmail.com>
2024-08-27 15:23:20 -0700

    Bump to 9.0.93

--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.89"
+$zipContentFolderName = "apache-tomcat-9.0.93"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
